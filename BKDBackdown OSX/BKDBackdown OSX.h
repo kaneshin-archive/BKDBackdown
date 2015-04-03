d34e51b49e0e0f93e49c4b70069badbd2f72d41a
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-04-03 22:42:14 +0900

    Add project for OSX

---
+++ BKDBackdown OSX/BKDBackdown OSX.h
@@ -0,0 +1,19 @@
+//
+//  BKDBackdown OSX.h
+//  BKDBackdown OSX
+//
+//  Created by Shintaro Kaneko on 4/3/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BKDBackdown OSX.
+FOUNDATION_EXPORT double BKDBackdown_OSXVersionNumber;
+
+//! Project version string for BKDBackdown OSX.
+FOUNDATION_EXPORT const unsigned char BKDBackdown_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BKDBackdown_OSX/PublicHeader.h>
+
+
